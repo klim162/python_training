41dd69e7b77588aa7c50b4f37f78ecd49fdb7718
klim162 <hanevold162@yandex.ru>
2025-05-01 14:40:52 +0700

    [test_add_group] переведен на pytest

--- test_add_group.py
+++ test_add_group.py
@@ -1,30 +1,20 @@
 # -*- coding: utf-8 -*-
-from selenium.common.exceptions import NoSuchElementException
-import unittest
+import pytest
 from group import Group
 from application import Application
 
-class TestAddGroup(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
     
-    def test_add_group(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_group(Group(name="dsfsaf", header="fsadfsa", footer="dsfasd"))
-        self.app.logaut()
+def test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="dsfsaf", header="fsadfsa", footer="dsfasd"))
+    app.logaut()
 
-    def test_add_empty_group(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_group(Group(name="", header="", footer=""))
-        self.app.logaut()
-
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def tearDown(self):
-        self.app.destroy()
-
-if __name__ == "__main__":
-    unittest.main()
+def test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logaut()
