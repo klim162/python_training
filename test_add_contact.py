c43628d3407ac65c9f8916bcd6eeb648e4f5e82b
klim162 <hanevold162@yandex.ru>
2025-05-01 14:53:45 +0700

    [test_add_contact] переведен на pytest

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,29 +1,19 @@
 # -*- coding: utf-8 -*-
-from selenium.common.exceptions import NoSuchElementException
-import unittest
+import pytest
 from contact import Contact
 from application import Application
 
-class TestAddContact(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
     
-    def test_add_contact(self):
-        self.app.login(username="admin", password="secret")
-        self.app.filling_fields(Contact(firstname="ewfweqf", middlename="erferfg", lastname="wfqwvdv",
-                            nickname="wfefr", title="wfewqc", company="cwef", address="wfeqrr",
-                            phonehome="2343", phonemobile="5432", phonework="34345", phonefax="3453",
-                            email="rr@tt.oo", email2="dsfd@err.yy", email3="dvdfwe@tt.yy", homepage="fewfrew",
-                            bday="28", bmonth="October", byear="4000", aday="28", amonth="October", ayear="1000"))
-        self.app.logaut()
-
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-
-    def tearDown(self):
-        self.app.destroy()
-
-if __name__ == "__main__":
-    unittest.main()
+def test_add_contact(app):
+    app.login(username="admin", password="secret")
+    app.filling_fields(Contact(firstname="ewfweqf", middlename="erferfg", lastname="wfqwvdv",
+                    nickname="wfefr", title="wfewqc", company="cwef", address="wfeqrr",
+                    phonehome="2343", phonemobile="5432", phonework="34345", phonefax="3453",
+                    email="rr@tt.oo", email2="dsfd@err.yy", email3="dvdfwe@tt.yy", homepage="fewfrew",
+                    bday="28", bmonth="October", byear="4000", aday="28", amonth="October", ayear="1000"))
+    app.logaut()
