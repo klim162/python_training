07ec587d1b629c53102e534cee9746375a5c9a82
klim162 <hanevold162@yandex.ru>
2025-05-14 16:28:39 +0700

    [group] добавлены проверки на сравнение содержимого

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -3,24 +3,36 @@ from model.group import Group
 
 def test_edit_first_group(app):
     old_groups = app.group.get_grout_list()
+    group = Group(name="name", header="header", footer="footer")
+    group.id = old_groups[0].id
     if app.group.count() == 0:
         app.group.create(Group(name="test_name"))
-    app.group.edit_first_group(Group(name="name", header="header", footer="footer"))
+    app.group.edit_first_group(group)
     new_groups = app.group.get_grout_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 def test_edit_first_group_name(app):
     old_groups = app.group.get_grout_list()
+    group = Group(name="New name")
+    group.id = old_groups[0].id
     if app.group.count() == 0:
         app.group.create(Group(name="test_name"))
-    app.group.edit_first_group(Group(name="New name"))
+    app.group.edit_first_group(group)
     new_groups = app.group.get_grout_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0].name = group.name
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 def test_edit_first_group_header(app):
     old_groups = app.group.get_grout_list()
+    group = Group(header="New header")
+    group.id = old_groups[0].id
     if app.group.count() == 0:
-        app.group.create(Group(name="test_name"))
-    app.group.edit_first_group(Group(header="New header"))
+        app.group.create(Group(header="test_header"))
+    app.group.edit_first_group(group)
     new_groups = app.group.get_grout_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0].header = group.header
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
