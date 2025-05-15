385ac0cf5fdbbf1a7ebe6c32d090391c7c8f402f
klim162 <hanevold162@yandex.ru>
2025-05-15 09:39:25 +0700

    [group] добавлены хэши/кэши

--- fixture/group.py
+++ fixture/group.py
@@ -32,6 +32,7 @@ class GroupHelper:
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_home_page()
+        self.group_cache = None
 
     def delete_first_group(self):
         wd = self.app.wd
@@ -41,6 +42,7 @@ class GroupHelper:
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_home_page()
+        self.group_cache = None
 
     def edit_first_group(self, new_group_data):
         wd = self.app.wd
@@ -52,6 +54,7 @@ class GroupHelper:
         # submit group update
         wd.find_element_by_name("update").click()
         self.return_home_page()
+        self.group_cache = None
 
     def select_ferst_group(self):
         wd = self.app.wd
@@ -67,12 +70,15 @@ class GroupHelper:
         self.open_groups_page()
         return len(wd.find_elements_by_name("selected[]"))
 
+    group_cache = None
+
     def get_grout_list(self):
-        wd = self.app.wd
-        self.open_groups_page()
-        groups = []
-        for element in wd.find_elements_by_css_selector("span.group"):
-            text = element.text
-            id = element.find_element_by_name("selected[]").get_attribute("value")
-            groups.append(Group(name=text, id=id))
-        return groups
+        if self.group_cache is None:
+            wd = self.app.wd
+            self.open_groups_page()
+            self.group_cache = []
+            for element in wd.find_elements_by_css_selector("span.group"):
+                text = element.text
+                id = element.find_element_by_name("selected[]").get_attribute("value")
+                self.group_cache.append(Group(name=text, id=id))
+        return list(self.group_cache)
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -8,8 +8,8 @@ def test_edit_first_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test_name"))
     app.group.edit_first_group(group)
+    assert len(old_groups) == app.group.count()
     new_groups = app.group.get_grout_list()
-    assert len(old_groups) == len(new_groups)
     old_groups[0] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
@@ -20,8 +20,8 @@ def test_edit_first_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test_name"))
     app.group.edit_first_group(group)
+    assert len(old_groups) == app.group.count()
     new_groups = app.group.get_grout_list()
-    assert len(old_groups) == len(new_groups)
     old_groups[0].name = group.name
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
@@ -32,7 +32,7 @@ def test_edit_first_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(header="test_header"))
     app.group.edit_first_group(group)
+    assert len(old_groups) == app.group.count()
     new_groups = app.group.get_grout_list()
-    assert len(old_groups) == len(new_groups)
     old_groups[0].header = group.header
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
