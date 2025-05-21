e54394db5ca9fcb3d5ea29f091dbeae7f2d3cebd
klim162 <hanevold162@yandex.ru>
2025-05-21 08:49:16 +0700

    [test_compare_contact] убрана чистка спецсимволов email на home page

--- test/test_compare_contact_filds.py
+++ test/test_compare_contact_filds.py
@@ -30,6 +30,5 @@ def merge_phones_like_on_home_page(contact):
 
 def merge_email_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "",
-                            map(lambda x: clear(x),
                                 filter(lambda x: x is not None,
-                                       [contact.email, contact.email2, contact.email3]))))
+                                       [contact.email, contact.email2, contact.email3])))
