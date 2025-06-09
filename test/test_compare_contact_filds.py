675eaf680b0430567a34025779b1aa3c6c475175
klim162 <hanevold162@yandex.ru>
2025-06-09 09:04:50 +0700

    [test_compare_contact_filds] переделан для проверки всех контактов с БД

--- test/test_compare_contact_filds.py
+++ test/test_compare_contact_filds.py
@@ -2,7 +2,7 @@ import re
 from random import randrange
 from model.contact import Contact
 
-def test_contact_filds_on_home_page(app):
+def test_contact_filds_on_home_page(app, db):
     if app.contact.count() == 0:
         app.contact.add_contact(Contact(firstname="ewfweqf", middlename="erferfg", lastname="wfqwvdv",
                     nickname="wfefr", title="wfewqc", company="cwef", address="wfeqrr",
@@ -10,25 +10,9 @@ def test_contact_filds_on_home_page(app):
                     email="rr@tt.oo", email2="dsfd@err.yy", email3="dvdfwe@tt.yy", homepage="fewfrew",
                     bday="28", bmonth="October", byear="4000", aday="28", amonth="October", ayear="1000"))
     contacts_from_home_page = app.contact.get_contact_list()
-    index = randrange(len(contacts_from_home_page))
-    contact_from_home_page = contacts_from_home_page[index]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
-    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
-    assert contact_from_home_page.address == contact_from_edit_page.address
-    assert contact_from_home_page.all_phones_from_homepage == merge_phones_like_on_home_page(contact_from_edit_page)
-    assert contact_from_home_page.all_mail_from_homepage == merge_email_on_home_page(contact_from_edit_page)
-
-def clear(s):
-    return re.sub("[() -]", "", s)
-
-def merge_phones_like_on_home_page(contact):
-    return "\n".join(filter(lambda x: x != "",
-                            map(lambda x: clear(x),
-                                filter(lambda x: x is not None,
-                                       [contact.phonehome, contact.phonemobile, contact.phonework]))))
-
-def merge_email_on_home_page(contact):
-    return "\n".join(filter(lambda x: x != "",
-                                filter(lambda x: x is not None,
-                                       [contact.email, contact.email2, contact.email3])))
+    def clear(contact):
+        return Contact(id=contact.id, firstname=contact.firstname.strip(), lastname=contact.lastname.strip(),
+                       address=contact.address, all_mail=contact.all_mail, all_phones=contact.all_phones)
+    contacts_from_db = db.get_contact_list()
+    assert (sorted(map(clear, contacts_from_db), key=Contact.id_or_max) ==
+            sorted(contacts_from_home_page, key=Contact.id_or_max))
