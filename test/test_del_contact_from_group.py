4e956258e6921036d30a2378be553cc0f82cc6c5
klim162 <hanevold162@yandex.ru>
2025-06-10 09:53:07 +0700

    Добавил предусловия в тесты с добавлением/удалением контакта из группы

--- test/test_del_contact_from_group.py
+++ test/test_del_contact_from_group.py
@@ -1,7 +1,13 @@
+from model.contact import Contact
+from model.group import Group
 import random
 
 
 def test_dell_contact_from_group(app, db, orm):
+    if len(db.get_contact_list()) == 0:
+        app.contact.add_contact(Contact(firstname="test_first"))
+    if len(db.get_group_list()) == 0:
+        app.group.create(Group(name="test_name"))
     contact = random.choice(db.get_contact_list())
     group = random.choice(db.get_group_list())
     if contact not in orm.get_contacts_in_group(group):
